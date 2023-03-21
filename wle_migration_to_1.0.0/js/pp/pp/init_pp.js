b67ee0bc0a868ffac1353f76dabd1d010464f3b8
Pipo <eliaducceschi@hotmail.it>
2023-03-21 10:16:01 +0100

    Convert to import/export progress

--- wle_migration_to_1.0.0/js/pp/pp/init_pp.js
+++ wle_migration_to_1.0.0/js/pp/pp/init_pp.js
@@ -1,9 +1,14 @@
-import { initEngineExtension } from "../plugin/extensions/wle/engine_extension"
 import { initArrayExtension } from "../plugin/extensions/js/array_extension"
+import { initMathExtension } from "../plugin/extensions/js/math_extension"
+import { initEngineExtension } from "../plugin/extensions/wle/engine_extension"
+import { initObjectExtension } from "../plugin/extensions/wle/object_extension"
+import { initSceneExtension } from "../plugin/extensions/wle/scene_extension"
 
 export function initPP(engine) {
-    initEngineExtension(engine);
     initArrayExtension();
+    initMathExtension();
 
-    //object extension bla bla
+    initEngineExtension(engine);
+    initObjectExtension(engine);
+    initSceneExtension(engine);
 }
