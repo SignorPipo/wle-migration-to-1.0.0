bcda04e0c34c39d208e71fd90db4bb77decdea91
Pipo <eliaducceschi@hotmail.it>
2023-03-22 12:12:10 +0100

    Refactor extension utils

--- wle_migration_to_1.0.0/js/pp/plugin/utils/extension_utils.js
+++ wle_migration_to_1.0.0/js/pp/plugin/utils/extension_utils.js
@@ -1,13 +1,15 @@
 export let ExtensionUtils = {
-    assignProperties: function (fromReference, toReference, enumerable = false, writable = true, configurable = true) {
-        let ownPropertyNames = Object.getOwnPropertyNames(fromReference);
-        for (let ownPropertyName of ownPropertyNames) {
-            Object.defineProperty(toReference, ownPropertyName, {
-                value: fromReference[ownPropertyName],
-                enumerable: enumerable,
-                writable: writable,
-                configurable: configurable
-            });
-        }
-    }
+    assignProperties
 };
+
+export function assignProperties(fromReference, toReference, enumerable = false, writable = true, configurable = true) {
+    let ownPropertyNames = Object.getOwnPropertyNames(fromReference);
+    for (let ownPropertyName of ownPropertyNames) {
+        Object.defineProperty(toReference, ownPropertyName, {
+            value: fromReference[ownPropertyName],
+            enumerable: enumerable,
+            writable: writable,
+            configurable: configurable
+        });
+    }
+}
