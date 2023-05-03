a79814ecf96112485a8bbb8899d9b73b573734d2
Pipo <eliaducceschi@hotmail.it>
2023-05-03 12:30:35 +0200

    Do not export not used classes

--- wle_migration_to_1.0.0/js/pp/gameplay/experimental/locomotion/cauldron/global_gravity_globals.js
+++ wle_migration_to_1.0.0/js/pp/gameplay/experimental/locomotion/cauldron/global_gravity_globals.js
@@ -3,34 +3,34 @@ import { Globals } from "../../../../pp/globals";
 let _myGlobalGravityAccelerations = new WeakMap();
 let _myGlobalGravityDirections = new WeakMap();
 
-export function getGlobalGravityAcceleration(engine = Globals.getMainEngine()) {
+function getGlobalGravityAcceleration(engine = Globals.getMainEngine()) {
     return _myGlobalGravityAccelerations.get(engine);
 }
 
-export function setGlobalGravityAcceleration(globalGravityAcceleration, engine = Globals.getMainEngine()) {
+function setGlobalGravityAcceleration(globalGravityAcceleration, engine = Globals.getMainEngine()) {
     _myGlobalGravityAccelerations.set(engine, globalGravityAcceleration);
 }
 
-export function removeGlobalGravityAcceleration(engine = Globals.getMainEngine()) {
+function removeGlobalGravityAcceleration(engine = Globals.getMainEngine()) {
     _myGlobalGravityAccelerations.delete(engine);
 }
 
-export function hasGlobalGravityAcceleration(engine = Globals.getMainEngine()) {
+function hasGlobalGravityAcceleration(engine = Globals.getMainEngine()) {
     return _myGlobalGravityAccelerations.has(engine);
 }
 
-export function getGlobalGravityDirection(engine = Globals.getMainEngine()) {
+function getGlobalGravityDirection(engine = Globals.getMainEngine()) {
     return _myGlobalGravityDirections.get(engine);
 }
 
-export function setGlobalGravityDirection(globalGravityDirection, engine = Globals.getMainEngine()) {
+function setGlobalGravityDirection(globalGravityDirection, engine = Globals.getMainEngine()) {
     _myGlobalGravityDirections.set(engine, globalGravityDirection);
 }
 
-export function removeGlobalGravityDirection(engine = Globals.getMainEngine()) {
+function removeGlobalGravityDirection(engine = Globals.getMainEngine()) {
     _myGlobalGravityDirections.delete(engine);
 }
 
-export function hasGlobalGravityDirection(engine = Globals.getMainEngine()) {
+function hasGlobalGravityDirection(engine = Globals.getMainEngine()) {
     return _myGlobalGravityDirections.has(engine);
 }
